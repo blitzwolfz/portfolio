3cd1c09530715c0c18f0e9554dfc0ac36c68f7f5
Sam <qureshisamin25@gmail.com>
2022-05-16 00:16:42 -0400

    Initial Setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Bar from './Bar/Bar';
+import Header from './Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header title='Hello there 🙋‍♂️'/>
+      <Bar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Bar/Bar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./Bar.css"
+
+export default function Bar() {
+
+  return (
+    <hr className='Bar'></hr>
+  )
+}
